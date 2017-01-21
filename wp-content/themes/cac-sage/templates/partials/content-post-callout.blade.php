2cf9ad17d4661a64ee21565c27612d42aa70c6e9
Alisa Herr <alisa@unitymakes.us>
2017-01-21 08:33:56 -0500

    Don't ignore distilled files and add migration plugin. Remove unused theme backup files.

--- wp-content/themes/cac-sage/templates/partials/content-post-callout.blade.php
+++
@@ -1,8 +0,0 @@
-<article class="post post-callout">
-    <figure>
-        @php(the_post_thumbnail())
-    </figure>
-    <h3 class="title">@php(the_title())</h3>
-    @php(the_excerpt())
-    <a href="@php(the_permalink())" class="btn btn-primary permalink">Read More</a>
-</article>
--- wp-content/themes/cac-sage/templates/partials/page-header.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-  <div class="page-header">
-    <h1>{!! App\title() !!}</h1>
-  </div>
-</div>
